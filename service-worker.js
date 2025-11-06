0eef22ce46f21ee8fd49784dfffad2da58ac8dd9
sako19870518-glitch <sako19870518@gmail.com>
2025-11-06 09:23:56 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "scale-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./book.xlsx"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
